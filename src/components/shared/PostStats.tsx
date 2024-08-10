9877819d27db80c510b30695b9117a9472378b0e
DominikBernatowicz <82055376+DominikBernatowicz@users.noreply.github.com>
2024-08-10 22:24:22 +0200

    adding functionality for liking and saving posts

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -1,37 +1,86 @@
+import { useDeleteSavedPost, useGetCurrentUser, useLikePost, useSavePost } from "@/lib/react-query/queriesAndMutatuions"
+import { checkIsLiked } from "@/lib/utils"
 import { Models } from "appwrite"
+import { useEffect, useState } from "react"
 
 type PostStatsProps = {
-    post: Models.Document,
-    userId: string
+  post: Models.Document,
+  userId: string
 }
 
 const PostStats = ({ post, userId }: PostStatsProps) => {
-    return (
-        <div className="flex justify-between items-center z-20">
-            <div className="flex gap-2 mr-5">
-                <img
-                    src="/assets/icons/like.svg"
-                    alt="like"
-                    width={20}
-                    height={20}
-                    onClick={() => { }}
-                    className="cursor-pointer"
-                />
-                <p className="small-medium lg: base-medium">0</p>
-            </div>
-            
-            <div className="flex gap-2">
-                <img
-                    src="/assets/icons/save.svg"
-                    alt="like"
-                    width={20}
-                    height={20}
-                    onClick={() => { }}
-                    className="cursor-pointer"
-                />
-            </div>
-        </div>
-    )
+  const likesList = post.likes.map((user: Models.Document) => user.$id)
+
+  const [likes, setLikes] = useState<string[]>(likesList)
+  const [isSaved, setIsSaved] = useState<boolean>(false)
+
+  const { mutate: likePost } = useLikePost()
+  const { mutate: savePost } = useSavePost()
+  const { mutate: deleteSavedPost } = useDeleteSavedPost()
+
+  const { data: currentUser } = useGetCurrentUser()
+
+  const savedPostRecord = currentUser?.save.find((record: Models.Document) => record.post.$id === post.$id)
+
+  const handleLikePost = (e: React.MouseEvent) => {
+    e.stopPropagation()
+
+    let newLikes = [...likes]
+
+    const hasLiked = newLikes.includes(userId)
+
+    if (hasLiked) {
+      newLikes = newLikes.filter(id => id !== userId)
+    } else {
+      newLikes.push(userId)
+    }
+
+    setLikes(newLikes)
+    likePost({ postId: post.$id, likesArray: newLikes })
+  }
+
+  const handleSavePost = (e: React.MouseEvent) => {
+    e.stopPropagation()
+
+    if (savedPostRecord) {
+      setIsSaved(false)
+      deleteSavedPost(savedPostRecord.$id)
+    } else {
+      savePost({ postId: post.$id, userId })
+      setIsSaved(true)
+    }
+  }
+
+  useEffect(() => {
+    setIsSaved(!!savedPostRecord)
+  }, [currentUser])
+
+  return (
+    <div className="flex justify-between items-center z-20">
+      <div className="flex gap-2 mr-5">
+        <img
+          src={`/assets/icons/${checkIsLiked(likes, userId) ? 'liked' : 'like'}.svg`}
+          alt="like"
+          width={20}
+          height={20}
+          onClick={handleLikePost}
+          className="cursor-pointer"
+        />
+        <p className="small-medium lg: base-medium">{likes.length}</p>
+      </div>
+
+      <div className="flex gap-2">
+        <img
+          src={`/assets/icons/${isSaved ? 'saved' : 'save'}.svg`}
+          alt="like"
+          width={20}
+          height={20}
+          onClick={handleSavePost}
+          className="cursor-pointer"
+        />
+      </div>
+    </div>
+  )
 }
 
 export default PostStats
--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,203 +1,276 @@
 import { INewPost, INewUser } from "@/types";
 import { account, appwriteConfig, avatars, databases, storage } from "./config";
 import { ID, ImageGravity, Query } from "appwrite";
+import { id } from "date-fns/locale";
 
 export async function createUserAccount(user: INewUser) {
-    try {
-        const newAccount = await account.create(
-            ID.unique(),
-            user.email,
-            user.password,
-            user.name
-        )
+  try {
+    const newAccount = await account.create(
+      ID.unique(),
+      user.email,
+      user.password,
+      user.name
+    )
 
-        if (!newAccount) throw Error
+    if (!newAccount) throw Error
 
-        const avatarUrl = avatars.getInitials(user.name)
+    const avatarUrl = avatars.getInitials(user.name)
 
-        const newUser = await saveUserToDB({
-            accountId: newAccount.$id,
-            email: newAccount.email,
-            name: newAccount.name,
-            username: user.username,
-            imageUrl: avatarUrl
-        })
+    const newUser = await saveUserToDB({
+      accountId: newAccount.$id,
+      email: newAccount.email,
+      name: newAccount.name,
+      username: user.username,
+      imageUrl: avatarUrl
+    })
 
-        return newUser
-    } catch (err) {
-        console.log(err)
-        return err
-    }
+    return newUser
+  } catch (err) {
+    console.log(err)
+    return err
+  }
 }
 
 export async function saveUserToDB(user: {
-    accountId: string,
-    email: string,
-    name: string,
-    imageUrl: URL,
-    username?: string
+  accountId: string,
+  email: string,
+  name: string,
+  imageUrl: URL,
+  username?: string
 }) {
-    try {
-        const newUser = await databases.createDocument(
-            appwriteConfig.databasesId,
-            appwriteConfig.userCollectionId,
-            ID.unique(),
-            user
-        )
+  try {
+    const newUser = await databases.createDocument(
+      appwriteConfig.databasesId,
+      appwriteConfig.userCollectionId,
+      ID.unique(),
+      user
+    )
 
-        return newUser
-    } catch (err) {
-        console.log(err)
-    }
+    return newUser
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 export async function signInAccount(user: {
-    email: string,
-    password: string
+  email: string,
+  password: string
 }) {
-    try {
-        // Sprawdzenie, czy istnieje aktywna sesja
-        const currentSession = await account.getSession('current').catch(() => null);
+  try {
+    // Sprawdzenie, czy istnieje aktywna sesja
+    const currentSession = await account.getSession('current').catch(() => null);
 
-        if (currentSession) {
-            console.log('Istnieje już aktywna sesja:', currentSession);
-            return currentSession;
-        }
-
-        // Utworzenie nowej sesji
-        const session = await account.createEmailPasswordSession(user.email, user.password);
-        return session;
-    } catch (err) {
-        console.log(err)
+    if (currentSession) {
+      console.log('Istnieje już aktywna sesja:', currentSession);
+      return currentSession;
     }
+
+    // Utworzenie nowej sesji
+    const session = await account.createEmailPasswordSession(user.email, user.password);
+    return session;
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 export async function signOutAccount() {
-    try {
-        const session = await account.deleteSession('current');
+  try {
+    const session = await account.deleteSession('current');
 
-        return session;
-    } catch (err) {
-        console.log(err)
-    }
+    return session;
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 export async function getCurrentUser() {
-    try {
-        const currentAccount = await account.get()
+  try {
+    const currentAccount = await account.get()
 
-        if (!currentAccount) throw Error
+    if (!currentAccount) throw Error
 
-        const currentUser = await databases.listDocuments(
-            appwriteConfig.databasesId,
-            appwriteConfig.userCollectionId,
-            [Query.equal('accountId', currentAccount.$id)]
-        )
+    const currentUser = await databases.listDocuments(
+      appwriteConfig.databasesId,
+      appwriteConfig.userCollectionId,
+      [Query.equal('accountId', currentAccount.$id)]
+    )
 
-        if (!currentUser) throw Error
+    if (!currentUser) throw Error
 
-        return currentUser.documents[0]
-    } catch (err) {
-        console.log(err)
-    }
+    return currentUser.documents[0]
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 export async function createPost(post: INewPost) {
-    try {
-        // Upload image to storage
-        const uploadedFile = await uploadFile(post.file[0])
+  try {
+    // Upload image to storage
+    const uploadedFile = await uploadFile(post.file[0])
 
-        if(!uploadedFile) throw Error
+    if (!uploadedFile) throw Error
 
-        // Get URL
-        const fileUrl = await getFilePreview(uploadedFile.$id)
+    // Get URL
+    const fileUrl = await getFilePreview(uploadedFile.$id)
 
-        if(!fileUrl) {
-            deleteFile(uploadedFile.$id)
-            throw Error
-        }
-
-        // Convert tags in an array
-        const tags = post.tags?.replace(/ /g,'').split(',').filter(tag => tag !== '') || []
-
-        // Save post to database
-        const newPost = await databases.createDocument(
-            appwriteConfig.databasesId,
-            appwriteConfig.postsCollectionId,
-            ID.unique(),
-            {
-                creator: post.userId,
-                caption: post.caption,
-                imageUrl: fileUrl,
-                imageId: uploadedFile.$id,
-                location: post.location,
-                tags,
-            }
-        )
-
-        if(!newPost) {
-            await deleteFile(uploadedFile.$id)
-            throw Error
-        }
-
-        return newPost
-    } catch (err) {
-        console.log(err)
+    if (!fileUrl) {
+      deleteFile(uploadedFile.$id)
+      throw Error
     }
+
+    // Convert tags in an array
+    const tags = post.tags?.replace(/ /g, '').split(',').filter(tag => tag !== '') || []
+
+    // Save post to database
+    const newPost = await databases.createDocument(
+      appwriteConfig.databasesId,
+      appwriteConfig.postsCollectionId,
+      ID.unique(),
+      {
+        creator: post.userId,
+        caption: post.caption,
+        imageUrl: fileUrl,
+        imageId: uploadedFile.$id,
+        location: post.location,
+        tags,
+      }
+    )
+
+    if (!newPost) {
+      await deleteFile(uploadedFile.$id)
+      throw Error
+    }
+
+    return newPost
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 export async function uploadFile(file: File) {
-    try {
-        const uploadFile = await storage.createFile(
-            appwriteConfig.storageId,
-            ID.unique(),
-            file
-        )
+  try {
+    const uploadFile = await storage.createFile(
+      appwriteConfig.storageId,
+      ID.unique(),
+      file
+    )
 
-        return uploadFile
-    } catch (err) {
-        console.log(err)
-    }
+    return uploadFile
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 export async function getFilePreview(fileId: string) {
-    try {
-        const fileUrl = storage.getFilePreview(
-            appwriteConfig.storageId,
-            fileId,
-            2000,
-            2000,
-            ImageGravity.Top,
-            100
-        )
+  try {
+    const fileUrl = storage.getFilePreview(
+      appwriteConfig.storageId,
+      fileId,
+      2000,
+      2000,
+      ImageGravity.Top,
+      100
+    )
 
-        return fileUrl
-    } catch (err) {
-        console.log(err)
-    }
+    return fileUrl
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 export async function deleteFile(fileId: string) {
-    try {
-        storage.deleteFile(
-            appwriteConfig.storageId,
-            fileId
-        )
+  try {
+    storage.deleteFile(
+      appwriteConfig.storageId,
+      fileId
+    )
 
-        return {status: 'ok'}
-    } catch (err) {
-        console.log(err)
-    }
+    return { status: 'ok' }
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 export async function getRecentPosts() {
-    const posts = await databases.listDocuments(
-        appwriteConfig.databasesId,
-        appwriteConfig.postsCollectionId,
-        [Query.orderDesc('$createdAt'), Query.limit(20)]
+  const posts = await databases.listDocuments(
+    appwriteConfig.databasesId,
+    appwriteConfig.postsCollectionId,
+    [Query.orderDesc('$createdAt'), Query.limit(20)]
+  )
+
+  if (!posts) throw Error
+
+  return posts
+}
+
+export async function likePost(postId: string, likesArray: string[]) {
+  try {
+    const updatePost = await databases.updateDocument(
+      appwriteConfig.databasesId,
+      appwriteConfig.postsCollectionId,
+      postId,
+      {
+        likes: likesArray
+      }
     )
 
-    if(!posts) throw Error
+    if (!updatePost) throw Error
 
-    return posts
+    return updatePost
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export async function savePost(postId: string, userId: string) {
+  try {
+    const updatePost = await databases.createDocument(
+      appwriteConfig.databasesId,
+      appwriteConfig.savesCollectionId,
+      ID.unique(),
+      {
+        user: userId,
+        post: postId
+      }
+    )
+
+    if (!updatePost) throw Error
+
+    return updatePost
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+
+export async function deleteSavedPost(savedRecordID: string) {
+  try {
+    const statusCode = await databases.deleteDocument(
+      appwriteConfig.databasesId,
+      appwriteConfig.savesCollectionId,
+      savedRecordID,
+    )
+
+    if (!statusCode) throw Error
+
+    return { status: 'ok' }
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export async function getPostById(postId: string) {
+  try {
+    const post = await databases.getDocument(
+      appwriteConfig.databasesId,
+      appwriteConfig.postsCollectionId,
+      postId
+    )
+
+    if(!post) throw Error
+
+    return post
+  } catch (err) {
+    console.log(err)
+  }
 }
