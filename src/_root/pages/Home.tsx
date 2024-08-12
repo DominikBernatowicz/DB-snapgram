48278404287a9ab5865ec82c4f801e55b929a8b2
DominikBernatowicz <82055376+DominikBernatowicz@users.noreply.github.com>
2024-08-12 12:49:13 +0200

    fix postcard  key

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,9 +1,7 @@
+import Loader from "@/components/shared/Loader";
 import PostCard from "@/components/shared/PostCard";
 import { useGetRecentPosts } from "@/lib/react-query/queriesAndMutatuions";
 import { Models } from "appwrite";
-import { Loader } from "lucide-react";
-import { useEffect, useState } from "react";
-
 const Home = () => {
   const { data: posts, isPending: isPostLoading, isError: isErrorPosts } = useGetRecentPosts()
 
@@ -12,12 +10,14 @@ const Home = () => {
       <div className="home-container">
         <div className="home-posts">
           <h2 className="h3-bold md:h2-bold text-left w-full">Home Feed</h2>
-          {isPostLoading && !posts ? (
+          {isPostLoading ? (
             <Loader />
+          ) : !posts ? (
+            <h2 className="h3-bold md:h2-bold text-center w-full">No posts</h2>
           ) : (
             <ul className="flex flex-col flex-1 gap-9 w-full">
-              {posts?.documents.map((post: Models.Document, index) => (
-                <PostCard post={post} key={index}/>
+              {posts?.documents.map((post: Models.Document) => (
+                <PostCard post={post} key={post.$id} />
               ))}
             </ul>
           )}
