66d3625f15c5272e4faf015b89eade3f55abb943
DominikBernatowicz <82055376+DominikBernatowicz@users.noreply.github.com>
2024-08-14 11:44:34 +0200

    Update Bottombar.tsx

--- src/components/shared/Bottombar.tsx
+++ src/components/shared/Bottombar.tsx
@@ -6,26 +6,29 @@ const Bottombar = () => {
   const { pathname } = useLocation()
 
   return (
-    <section className='bottom-bar'>
-      {bottombarLinks.map((link, index) => {
-        const isActive = pathname === link.route
-
-        return (
-          <Link
-            to={link.route}
-            key={index}
-            className={`bottombar-link group ${isActive && 'bg-primary-500 rounded-[10px]'} flex-center flex-col gap-1 p-2 transition`}
-          >
-            <img
-              src={link.imgURL}
-              alt={link.label}
-              className={`group-hover:invert-white ${isActive && 'invert-white'}`}
-            />
-            <p></p>
-            {link.label}
-          </Link>
-        )
-      })}
+    <section className='bottom-bar h-24'>
+      <ul className='flex w-full justify-around'>
+        {bottombarLinks.map((link, index) => {
+          const isActive = pathname === link.route
+          return (
+            <li key={index} className='relative list-none z-10 text-center'>
+              <Link
+                to={link.route}
+                className={`relative flex justify-center items-center flex-col h-14 w-14 text-center font-medium rounded-full transition-all duration-500 ease-in-out ${isActive ? 'bg-primary-500 -translate-y-7' : 'translate-y-0'}`}
+                >
+                <img
+                  src={link.imgURL}
+                  alt={link.label}
+                  className={`h-8 w-8 relative block leading-6 text-[1.5em] text-center ${isActive && 'invert-white'}`}
+                />
+              </Link>
+              <p className={`absolute bottom-0 left-1/2 transform -translate-x-1/2 transition-all duration-500 ease-in-out ${isActive ? 'translate-y-0 opacity-100' : 'translate-y-7 opacity-0'}`}>
+                {link.label}
+              </p>
+            </li>
+          )
+        })}
+      </ul>
     </section>
   )
 }
