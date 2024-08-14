35a391e9f99971b0ef073843bc881e092a501f28
DominikBernatowicz <82055376+DominikBernatowicz@users.noreply.github.com>
2024-08-14 20:14:10 +0200

    Add icon

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -34,7 +34,16 @@ const Explore = () => {
   return (
     <div className="explore-container">
       <div className="explore-inner_container">
-        <h2 className='h3-bold md:h2-bold w-full'>Search Posts</h2>
+        <div className="max-w-5xl flex-start gap-3 justify-start w-full">
+          <img
+            src="/assets/icons/file-upload.svg"
+            width={36}
+            height={36}
+            alt="search"
+            className='invert-white'
+          />
+          <h2 className="h3-bold md:h2-bold w-full">Search Posts</h2>
+        </div>
         <div className="flex gap-1 w-full rounded-lg bg-dark-4">
           <img
             src="/assets/icons/search.svg"
